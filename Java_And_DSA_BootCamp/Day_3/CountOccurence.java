77f7bc0cfb22059be5c5eaf924555c7f123a4dd3
Kumaravel <kumaravel4502q@gmail.com>
2025-05-30 09:50:02 +0530

    Updated CountOccurence logic

--- Java_And_DSA_BootCamp/Day_3/CountOccurence.java
+++ Java_And_DSA_BootCamp/Day_3/CountOccurence.java
@@ -5,22 +5,24 @@ public class CountOccurence {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter t number of terms in the series:");
         int n = s.nextInt();
+        System.out.println("Enter the number to count the occurrence : ");
+        int numberToCount = s.nextInt();
         int rem, count = 0;
         // while (n > 0) {
         // rem = n % 10;
-        // if (rem == 3) {
+        // if (rem == numberToCount) {
         // count++;
         // }
         // n = n / 10;
         // }
         for (int temp = n; temp > 0; temp = temp / 10) {
             rem = temp % 10;
-            if (rem == 3) {
+            if (rem == numberToCount) {
                 count++;
             }
 
         }
-        System.out.println("The number of 3's in " + n + " is: " + count); // s.close();
+        System.out.println("The number of " + numberToCount + "'s in " + n + " is: " + count); // s.close();
 
     }
 }
